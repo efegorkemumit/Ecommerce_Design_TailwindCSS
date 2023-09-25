121aa47407e13d7737608b7a6305dae23e97c660
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-09-25 18:05:41 +0300

    Screens
    Youtube Efe Görkem Ümit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
+    screens:{
+      sm:'576px',
+      md:'768px',
+      lg:'992px',
+      xl:'1200px',
+
+    },
+    container:{
+      center:true,
+      padding: '1rem'
+    },
     extend: {
       fontFamily: {
         'poppins' : ['Poppins'],
